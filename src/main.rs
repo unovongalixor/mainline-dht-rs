e72f457921adad7169112a7c1061fd71889e8374
Simon Bursten <smnbursten@gmail.com>
2022-01-15 15:25:15 +0200

    added impl for ping query and handled warnings

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,8 @@ use clap::App;
 use ansi_term::Colour;
 use ansi_term::Style;
 use queries::PingRequest;
-use queries::PingRequestPayload;
 use std::net::UdpSocket;
 use std::time::Duration;
-use std::net::ToSocketAddrs;
-use std::str;
-
 
 
 fn main() {
@@ -28,30 +24,17 @@ fn main() {
         String::from("router.bittorrent.com:6881")
     ];
 
-    let ping_request = PingRequest {
-        t: String::from("b7"),
-        y: String::from("q"),
-        q: String::from("ping"),
-        a: PingRequestPayload {
-            id: String::from("abcdefghij0123456789")
-        }
-    };
+    let ping_request = PingRequest::new(String::from("abcdefghij0123456789")); 
     let string = serde_bencoded::to_vec(&ping_request).unwrap();
 
     println!("{}", std::str::from_utf8(&string).unwrap());
 
     let socket = UdpSocket::bind("0.0.0.0:33333").expect("failed to bind host socket");
-    socket.set_read_timeout(Some(Duration::new(5, 0)));
-    println!("Broadcast: {:?}", socket.broadcast());
-    println!("Timeout: {:?}", socket.read_timeout());
-
+    socket.set_read_timeout(Some(Duration::new(5, 0))).expect("failed to set read timeout");
     match socket.send_to(&string, bootstrap_servers[0].clone()) {
         Ok(n) => {
             if n != string.len() {
                 return
-            }
-            else {
-                // Do nothing because we sent the number of bytes we expected to send
             }
         },
         Err(e) => {
@@ -66,6 +49,6 @@ fn main() {
     while let Ok((n, addr)) = socket.recv_from(&mut recv_buff) {
         println!("{} bytes response from {:?}", n, addr);
         println!("{}", String::from_utf8_lossy(&recv_buff));
-        // Remaining code not directly relevant to the question
+        return
     }
 }
--- src/queries.rs
+++ src/queries.rs
@@ -8,7 +8,25 @@ pub struct PingRequest {
     pub a: PingRequestPayload
 }
 
+impl PingRequest {
+    pub fn new(id: String) -> PingRequest {
+        let r = PingRequest {
+            t: String::from("b7"),
+            y: String::from("q"),
+            q: String::from("ping"),
+            a: PingRequestPayload::new(id)
+        };
+        return r;
+    }
+}
+
 #[derive(Debug, Serialize, Deserialize)]
 pub struct PingRequestPayload {
     pub id: String
+}
+
+impl PingRequestPayload {
+    fn new(id: String) -> PingRequestPayload {
+        PingRequestPayload { id:String::from(id) }
+    }
 }
